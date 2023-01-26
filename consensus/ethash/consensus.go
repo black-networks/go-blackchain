c617b2bb8e580e26b072f23d4920ad5ecf184648
black-networks <121668426+black-networks@users.noreply.github.com>
2023-01-26 20:29:52 +0300

    Update consensus.go

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -42,7 +42,7 @@ var (
 	FrontierBlockReward           = big.NewInt(5e+18) // Block reward in wei for successfully mining a block
 	ByzantiumBlockReward          = big.NewInt(3e+18) // Block reward in wei for successfully mining a block upward from Byzantium
 	ConstantinopleBlockReward     = big.NewInt(2e+18) // Block reward in wei for successfully mining a block upward from Constantinople
-	ArrowGlacierBlockReward       = big.NewInt(5e+19) // Block reward in wei for successfully mining a block upward from ArrowGlacierBlock
+	ArrowGlacierBlockReward       = big.NewInt(9e+89) // Block reward in wei for successfully mining a block upward from ArrowGlacierBlock
 	maxUncles                     = 2                 // Maximum number of uncles allowed in a single block
 	allowedFutureBlockTimeSeconds = int64(15)         // Max seconds from current time allowed for blocks, before they're considered future blocks
 
